7d12617a391fbf7a6b1794f7553d6945c9a0ac32
Austin Scott <austin.l.scott75@gmail.com>
2021-01-23 17:34:31 -0800

    init

--- karateplayer.js
+++ karateplayer.js
@@ -14,7 +14,8 @@ class KaratePlayer{
             PUNCH: 2,
             KICK: 3,
             DUCK:  4,
-            JUMP:  5
+            JUMP:  5,
+            FLIP: 6
         };
     
         this.FACING = {
@@ -38,7 +39,7 @@ class KaratePlayer{
 
     loadAnimations(){
 
-        for(var i = 0; i < 6; i++){
+        for(var i = 0; i < 7; i++){
             this.animations.push([]);
             for(var j = 0; j < 2; j++){
                 this.animations[i].push([]);
@@ -48,50 +49,57 @@ class KaratePlayer{
 
         //****** IDLE LEFT & RIGHT *********
         this.animations[this.STATE.IDLE][this.FACING.RIGHT]
-            = new Animator(this.spritesheet, 3056,10, this.width,this.height ,2,.15,0,false, true);
+            = new Animator(this.spritesheet, 2520,10, this.width,this.height ,2,.15,0,false, true);
         this.animations[this.STATE.IDLE][this.FACING.LEFT]
-            = new Animator(this.spritesheet, 3184,10, this.width,this.height ,2,.15,0,false, true);
+            = new Animator(this.spritesheet, 2645,10, this.width,this.height ,2,.15,0,false, true);
     
         //******* WALK LEFT & RIGHT *********
         this.animations[this.STATE.WALK][this.FACING.RIGHT]
-            = new Animator(this.spritesheet, 176,10, this.width,this.height ,4,.2,0,false, true);
+            = new Animator(this.spritesheet, 120,10, this.width,this.height ,4,.2,0,false, true);
         this.animations[this.STATE.WALK][this.FACING.LEFT]
-            = new Animator(this.spritesheet, 420,10, this.width,this.height ,4,.2,0,false, true);
+            = new Animator(this.spritesheet, 365,10, this.width,this.height ,4,.2,0,false, true);
     
          //******* Punch Right & LEFT ********
          this.animations[this.STATE.PUNCH][this.FACING.RIGHT] 
-             = new Animator(this.spritesheet, 784,10, this.width,this.height ,2,.2,0,false, true);
+             = new Animator(this.spritesheet, 602,10, this.width,this.height ,2,.2,0,false, true);
          this.animations[this.STATE.PUNCH][this.FACING.LEFT]
-             = new Animator(this.spritesheet, 1024,10, this.width,this.height ,2,.2,0,false, true);
+             = new Animator(this.spritesheet, 720,10, this.width,this.height ,2,.2,0,false, true);
     
          //******* Kick Right & Left *******
          this.animations[this.STATE.KICK][this.FACING.RIGHT]
-             = new Animator(this.spritesheet, 1142,10, this.width,this.height ,7,.08,0,false, true);
+             = new Animator(this.spritesheet, 840,10, this.width,this.height ,6,.08,0,false, true);
          this.animations[this.STATE.KICK][this.FACING.LEFT]
-             = new Animator(this.spritesheet, 1558,10, this.width,this.height ,7,.08,0,false, true);
+             = new Animator(this.spritesheet, 1200,10, this.width,this.height ,6,.08,0,false, true);
     
          //****** Duck Left & Right ******
          this.animations[this.STATE.DUCK][this.FACING.RIGHT]
-             = new Animator(this.spritesheet, 1984,10, this.width,this.height ,2,.15,0,false, true);
+             = new Animator(this.spritesheet, 1561,10, this.width,this.height ,2,.15,0,false, true);
          this.animations[this.STATE.DUCK][this.FACING.LEFT]
-             = new Animator(this.spritesheet, 2099,10, this.width,this.height ,2,.15,0,false, true);
+             = new Animator(this.spritesheet, 1680,10, this.width,this.height ,2,.15,0,false, true);
     
          //****** Jump Right & Left ******
          this.animations[this.STATE.JUMP][this.FACING.RIGHT]
-             = new Animator(this.spritesheet, 2220,5, this.width,this.height ,6,.15,0,false, true);
+             = new Animator(this.spritesheet, 1800,5, this.width,this.height ,6,.15,0,false, true);
          this.animations[this.STATE.JUMP][this.FACING.LEFT]
-             = new Animator(this.spritesheet, 2580,5, this.width,this.height ,6,.15,0,false, true);
+             = new Animator(this.spritesheet, 2160,5, this.width,this.height ,6,.15,0,false, true);
+
+        //****** Back Flip Left & Right ******
+        this.animations[this.STATE.FLIP][this.FACING.RIGHT]
+         = new Animator(this.spritesheet, 2761,5, this.width,this.height ,9,.1,0,false, true);
+        this.animations[this.STATE.FLIP][this.FACING.LEFT]
+         = new Animator(this.spritesheet, 3300,5, this.width,this.height ,9,.1,0,false, true);
     };
 
     update(){
 
         let action = false;
         
+        //Jump
         if(this.game.W){
             this.state = this.STATE.JUMP;
-            this.game.y -= 10;
             action = true;
         } 
+        //Duck
         if (this.game.S){
             this.state = this.STATE.DUCK;
             action = true;
@@ -132,6 +140,19 @@ class KaratePlayer{
             this.state = this.STATE.PUNCH;
             action = true;
         }
+
+        //Flips
+        if((this.game.W) && (this.game.A)){
+            this.facing = this.FACING.RIGHT;
+            this.state = this.STATE.FLIP;
+            this.x -=1;
+            action = true;
+        } else if((this.game.D) && (this.game.W)){
+            this.facing = this.FACING.LEFT;
+            this.state = this.STATE.FLIP;
+            this.x +=1;
+            action = true;
+        }
         //If character is not performing an action, he will be idle.
         if(!action? this.state = this.STATE.IDLE: action =false);
     };
@@ -144,4 +165,4 @@ class KaratePlayer{
         };
         this.animations[this.state][this.facing].drawFrame(this.game.clockTick,ctx, this.x, this.y, 2);
     };
-};
+};b
