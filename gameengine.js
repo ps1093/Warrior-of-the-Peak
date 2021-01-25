fff6bdf9d74a48f90906fa1b214830ebb2898d13
ps93 <paras93sharma@yahoo.com>
2021-01-24 17:21:11 -0800

    CatPlayer & background

--- gameengine.js
+++ gameengine.js
@@ -17,6 +17,8 @@ class GameEngine {
         this.D = false;
         this.P = false;
         this.C = false;
+        this.E = false;
+        this.R = false; 
 
     };
 
@@ -63,6 +65,15 @@ class GameEngine {
                     that.P = true;
                     break;
 
+                case "KeyE":
+                    that.E = true;
+                    console.log("puching");
+                    break;
+                case "KeyR":
+                    that.R = true;
+                    console.log("kicking");
+                    break;
+
             }
         }, false);
 
@@ -89,6 +100,15 @@ class GameEngine {
                     break;
                 case "Period":
                     that.P = false;
+                    break;
+
+                case "KeyE":
+                    that.E = false;
+                    console.log("puching");
+                    break;
+                    case "KeyR":
+                    that.R = false;
+                    console.log("kicking");
                     break;
 
             }
--- scenemanager.js
+++ scenemanager.js
@@ -11,10 +11,14 @@ class SceneManager{
     };
 
     loadLevel(){
-        let ground = new Ground(this.game, 0, 721, 1024);
-        this.game.addEntity(ground);
+        // let ground = new Ground(this.game, 0, 721, 1024);
+        // this.game.addEntity(ground);
 
-        this.karateplayer = new KaratePlayer(this.game, 0, 475);
+        this.bkground = new background(this.game, 0, 0);
+        this.game.addEntity(this.bkground);
+        this.catplayer = new catplayer(this.game, 400, 435 );
+        this.game.addEntity(this.catplayer);
+        this.karateplayer = new KaratePlayer(this.game, 0, 375);
         this.game.addEntity(this.karateplayer);
     };
 
