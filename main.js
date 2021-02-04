51e83967919959d7e264ffd131ffa766e05b9a3f
Suk Won <Swon206@UW.edu>
2021-02-04 12:25:10 -0800

    merge from suk

--- main.js
+++ main.js
@@ -13,13 +13,9 @@ ASSET_MANAGER.queueDownload("./sprites/falls.png");
 
 ASSET_MANAGER.queueDownload("./sprites/backdrop.png");
 
-<<<<<<< HEAD
 ASSET_MANAGER.queueDownload("./sprites/ChunLi.png");
 
 ASSET_MANAGER.queueDownload("./sprites/BillyLee.png");
-=======
-ASSET_MANAGER.queueDownload("./sprites/round1.png");
->>>>>>> fef43a8d8e53b0599993cbdc81966bfa7df61e9e
 
 ASSET_MANAGER.downloadAll(function () {
 	var canvas = document.getElementById('gameWorld');
