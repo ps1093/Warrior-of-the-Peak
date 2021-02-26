9a1eddad399cee0fcd6b5fae7bd4b0229f46e73c
Austin Scott <austin.l.scott75@gmail.com>
2021-02-26 14:18:03 -0800

    init

--- scenemanager.js
+++ scenemanager.js
@@ -104,19 +104,19 @@ class SceneManager{
                 break;
             //Yodha
             case this.Players.CHARACTERS[2]:
-                this.opponent = new catplayer(this.game, 0, 0);
+                this.opponent = new catplayer(this.game, 960, 0, this.player, this.Players.CHARACTERS[2]);
                 break;
             //Chun Li
             case this.Players.CHARACTERS[3]:
-                this.opponent = new ChunLi(this.game, 960, 0, this.Players.CHARACTERS[3]);
+                this.opponent = new ChunLi(this.game, 960, 0,  this.player, this.Players.CHARACTERS[3]);
                 break;
             //Billy Lee
             case this.Players.CHARACTERS[4]:
-                this.opponent = new BillyLee(this.game, 960, 0, this.Players.CHARACTERS[4]);
+                this.opponent = new BillyLee(this.game, 960, 0, this.player, this.Players.CHARACTERS[4]);
                 break;
               //Goku
             case this.Players.CHARACTERS[5]:
-                this.opponent = new Goku(this.game, 960, 0, this.Players.CHARACTERS[5]);
+                this.opponent = new Goku(this.game, 960, 0, this.player, this.Players.CHARACTERS[5]);
                 break;
         }
         this.title = title;
